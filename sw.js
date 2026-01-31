0bd527679579f4df363da3b53a3f0a2a9deb969d
thinkeasyacademy <onlinepathshala98@gmail.com>
2026-01-31 09:44:41 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,54 @@
+
+const CACHE_NAME = 'think-easy-v3';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3',
+  'https://assets.mixkit.co/active_storage/sfx/2019/2019-preview.mp3'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Listener for system notifications triggered from background
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      if (clientList.length > 0) {
+        let client = clientList[0];
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i];
+          }
+        }
+        return client.focus();
+      }
+      return clients.openWindow('/');
+    })
+  );
+});
